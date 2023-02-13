7c90769d75626682cf77bdd5ccc7ac6c88fde63a
박경호 <qkrrudgh30@google.com>
2023-02-13 16:55:59 +0900

    Initialize a repo

---
+++ Source/ZombieSurvival/ZombieSurvivalGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ZombieSurvivalGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ZOMBIESURVIVAL_API AZombieSurvivalGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
